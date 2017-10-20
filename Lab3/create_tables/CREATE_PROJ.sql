533a705399661d0b8984cf62fa3f392f4d084c5c
Aaron Chamberlain <aaronjchamberlain@gmail.com>
2017-10-20 09:57:20 -0700

    Lab 3: Final versions, PROJ now hass a PK, FKs set in all Alter Tables

--- Lab3/create_tables/CREATE_PROJ.sql
+++ Lab3/create_tables/CREATE_PROJ.sql
@@ -1,7 +1,7 @@
 /* File to create PROJ Table */
 CREATE TABLE PROJ
 (
-  projNumber NUMBER(2) NOT NULL UNIQUE,
+  projNumber NUMBER(2) PRIMARY KEY NOT NULL,
   projName VARCHAR2(30),
   deptNum NUMBER(2),
   CONSTRAINT CHK_projNumber CHECK (projNumber BETWEEN 10 AND 99),
